77d068881a3925247dc32ac9b15ca0badd6f0b9e
Alex Hauber <alexhauber@yahoo.com>
2019-05-17 13:03:24 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import url
 
-from fir_todos import views
+from incidents import views
 
 urlpatterns = [
-    url(r'^(?P<incident_id>\d+)/list/$', views.list, name='list'),
-    url(r'^(?P<incident_id>\d+)/create/$', views.create, name='create'),
-    url(r'^(?P<todo_id>\d+)/delete/$', views.delete, name='delete'),
-    url(r'^(?P<todo_id>\d+)/toggle_status/$', views.toggle_status, name='toggle_status'),
-    url(r'^tasks/$', views.dashboard, name='dashboard'),
+    url(r'^$', views.index, {'is_incident': True}, name='index'),
+    url(r'^all/$', views.incidents_all, name='all'),
+    url(r'^(?P<incident_id>\d+)/$', views.details, name='details'),
+    url(r'^(?P<incident_id>\d+)/followup/$', views.followup, name='followup'),
+    url(r'^(?P<incident_id>\d+)/comment/$', views.comment, name='comment'),
+    url(r'^(?P<incident_id>\d+)/comment/(?P<comment_id>\d+)/delete/$', views.delete_comment, name='delete_comment'),
+    url(r'^(?P<incident_id>\d+)/edit/$', views.edit_incident, name='edit'),
+    url(r'^(?P<incident_id>\d+)/delete/$', views.delete_incident, name='delete'),
+    url(r'^(?P<incident_id>\d+)/status/(?P<status>[OBC])$', views.change_status, name='change_status'),
+    url(r'^(?P<incident_id>\d+)/attribute$', views.add_attribute, name='add_attribute'),
+    url(r'^(?P<incident_id>\d+)/attribute/(?P<attribute_id>\d+)/delete/$', views.delete_attribute, name='delete_attribute'),
 ]
